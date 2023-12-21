a410ccae955238d50c04cb52b7e5a06573535903
andika wahyudi <andikawpb200@gmail.com>
2023-12-21 12:23:57 +0700

    Setup Project

---
+++ server/src/main/java/com/netheve/andikagunshop/AndikaGunshopApplication.java
@@ -0,0 +1,13 @@
+package com.netheve.andikagunshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndikaGunshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndikaGunshopApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/netheve/andikagunshop/AndikaGunshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netheve.andikagunshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndikaGunshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
